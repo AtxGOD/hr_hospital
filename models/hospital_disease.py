d94843d212ecd97676a2c6eca04d0f9329cf4094
AtxGOD <vista2990@gmail.com>
2023-02-22 16:32:13 +0200

    init 2

--- models/hospital_disease.py
+++
@@ -1,8 +0,0 @@
-from odoo import fields, models
-
-
-class HospitalDisease(models.Model):
-    _name = 'hospital.disease'
-    _description = 'Disease'
-
-    name = fields.Char(string='Disease name', required=True)
